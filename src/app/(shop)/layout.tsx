ece5f49620578f8cf4257710ebb4a0ee0c2e7bae
Rdani2005 <daniel.sequeira@betanysports.eu>
2024-02-23 08:28:08 -0600

    Added sidebar menu

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,14 +1,16 @@
-import { TopMenu } from "@/components";
+import { TopMenu, Sidebar } from "@/components";
 
 export default function ShopLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <main className=" min-h-screen ">
-            <TopMenu />
-            <div className="px-0 sm:px-10">{children}</div>
-        </main>
-    );
+  return (
+    <main className="min-h-screen">
+      <TopMenu />
+      <Sidebar />
+
+      <div className="px-0 sm:px-10">{children}</div>
+    </main>
+  );
 }
--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,33 +1,35 @@
+"use client";
+
 import { Product } from "@/interfaces/product.interface";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 interface ProductGridItemProps {
-    product: Product;
+  product: Product;
 }
 
 export const ProductGridItem = ({ product }: ProductGridItemProps) => {
-    return (
-        <div className="rounded-md overflow-hidden fade-in">
-            <Link href={`/product/${product.slug}`}>
-                <Image
-                    src={`/products/${product.images[0]}`}
-                    alt={product.title}
-                    className="w-full object-cover"
-                    width={500}
-                    height={500}
-                />
-            </Link>
-            <div className="p-4 flex flex-col">
-                <Link
-                    href={`/product/${product.slug}`}
-                    className="hover:text-blue-500"
-                >
-                    {product.title}
-                </Link>
-                <span className="font-bold">${product.price}</span>
-            </div>
-        </div>
-    );
+  const [displayImage, setDisplayImage] = useState(product.images[0]);
+  return (
+    <div className="rounded-md overflow-hidden fade-in">
+      <Link href={`/product/${product.slug}`}>
+        <Image
+          src={`/products/${displayImage}`}
+          alt={product.title}
+          className="w-full object-cover rounded"
+          width={500}
+          height={500}
+          onMouseEnter={() => setDisplayImage(product.images[1])}
+          onMouseLeave={() => setDisplayImage(product.images[0])}
+        />
+      </Link>
+      <div className="p-4 flex flex-col">
+        <Link href={`/product/${product.slug}`} className="hover:text-blue-500">
+          {product.title}
+        </Link>
+        <span className="font-bold">${product.price}</span>
+      </div>
+    </div>
+  );
 };
